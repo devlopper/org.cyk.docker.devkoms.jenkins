6474b17b5532beb4a7bec789e85950b54c50e37b
devlopper <kycdev@gmail.com>
2022-04-01 19:09:48 +0000

    commit

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ else
   echo "Container cyk-jenkins has been removed."
 fi
 
-sudo docker create --name cyk-jenkins -p 9000:8080 -p 50000:50000 -m 4g -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --restart always cyk-jenkins:jdk11
+sudo docker create --name cyk-jenkins -p 9000:8080 -p 50000:50000 -m 4g -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /home:/home --restart always cyk-jenkins:jdk11
 sudo docker start cyk-jenkins
